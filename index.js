57efb9c58a79aca0a4e2f3f4ec3efb1abba5e997
Erica Domerese <xrabbitonthemoonx@gmail.com>
2024-05-31 15:55:28 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
+let num1 = 1;
+let num2 = 62;
+let multiply = num1 * num2;
+console.log(multiply); 
+ let num3 = 24
+ let num4 = 5
+ let random= Math.floor(Math.random() * 100) + 1;
+ let mod = num3 % num4
+ 
+ 
+ 
+ 
+ const numbers = [10, 20, 5, 15];
+    const max = Math.max(...numbers);
+Number.isNan(Nan);
+//=> true
 
-// Write your code here
+    
+
+
+    
+
+
+    
+    console.log(maxNumber); // This will output 20
+
+
